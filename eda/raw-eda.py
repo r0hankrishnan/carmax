65be583cfabc66cfd457e17153b4fde9abf3053c
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-07-24 00:39:34 -0400

    Update raw-eda.py

--- eda/raw-eda.py
+++ eda/raw-eda.py
@@ -1,6 +1,48 @@
 #Modules
 import pandas as pd
 import numpy as np 
+import os
+
+#Check cwd
+os.getcwd()
 
 #Data
-df = pd.read_csv("/data/carmax.csv")
+df = pd.read_csv("../data/carmax.csv")
+
+#Basic info about df
+df.columns
+df.shape
+df.dtypes
+for col in df.columns:
+    if df[col].dtype == "object" or df[col].dtype == "string":
+        print(col)
+
+df["color"].unique()
+df.describe()
+df.isna().sum()
+
+#Copy df and remove NAs
+df1 = df.copy()
+df1 = df1.dropna()
+print(f"df shape: {df.shape} vs df1 shape: {df1.shape}")
+
+#Check that NAs were removed
+for col in df1.columns:
+    if df1[col].isna().sum() != 0:
+        print(f"{col} has {df1[col].isna().sum()} NA values")
+
+df1.describe()
+df1.nunique()
+
+#Single out columns with under 10 unique values
+catList = []
+for col in df1.columns:
+    if df1[col].nunique() <= 10:
+        print(f"{col} has {df[col].nunique()} distinct values.")
+        catList.append(col)
+        
+catList #List of variables that need encoding
+
+        
+        
+    
