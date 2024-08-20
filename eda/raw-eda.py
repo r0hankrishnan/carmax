ac9b0ab02831add97f511d48616494a78a957c49
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-08-19 23:26:25 -0400

    Update raw-eda.py
    Start converting columns to dummy or float

--- eda/raw-eda.py
+++ eda/raw-eda.py
@@ -17,7 +17,11 @@ for col in df.columns:
     if df[col].dtype == "object" or df[col].dtype == "string":
         print(col)
 
-df["color"].unique()
+df["color"].unique() 
+df["model_year_appraisal"].unique() #1992-2014
+df["model_year_appraisal"].nunique()
+#Can condense this list of colors into Gray, Black, White, 
+# Red, Blue, Brown, Green, Orange, Yellow, Purple, Silver, Gold
 df.describe()
 df.isna().sum()
 
@@ -43,14 +47,45 @@ for col in df1.columns:
         
 catList #List of variables that need encoding
 
+df1.head()
+df1["make_appraisal"].unique()
+df1["model_appraisal"].unique()
+df1["engine_appraisal"].unique()
+
+def removeL(value):
+    return value[0:3]
+
+df2 = df1.copy()
+df2["engine_appraisal"]= df2["engine_appraisal"].apply(removeL).astype(float)
+df2["engine"] = df2["engine"].apply(removeL).astype(float)
+
+df2 = df2.rename(columns = {"trim_level":"trim_level_premium",
+                            "trim_level_appraisal":"trim_level_appraisal_premium"})
+
+df2.columns
+
+df2["trim_level_premium"] = pd.get_dummies(df2["trim_level_premium"])["Premium"]
+df2["trim_level_appraisal_premium"] = pd.get_dummies(df2["trim_level_appraisal_premium"])["Premium"]
+
+df2[["trim_level_premium", "trim_level_appraisal_premium"]]
+
+df2.groupby("online_appraisal_flag").mean(numeric_only=True)
+df2["model_year"].nunique()
+
 #pip install plotly
 #pip install nbformat
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import nbformat
 
 fig = px.histogram(df, x="price", color="trim_level")
 fig.show()
 
-fig = px.d
+fig = px.scatter(df, x = "price", y = "appraisal_offer", size="days_since_offer", opacity=0.6)
+fig.select_legends()
+fig.show()
+
+
     
