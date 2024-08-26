f03caba213056167e752fe45ed1d1601c5ac67e9
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-08-26 19:39:07 -0400

    Update lasso.py

--- models/lasso.py
+++ models/lasso.py
@@ -1,1 +1,15 @@
 #https://machinelearningmastery.com/lasso-regression-with-python/
+#https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LassoCV.html
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LassoCV, ElasticNetCV
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Load data
+
+#Scale features
+
+#Split data
+
