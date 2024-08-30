257cf225fa64ac7971631929c2993ae361f17104
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-08-29 23:06:18 -0400

    Update 4_[Experimental].py

--- streamlit/pages/4_[Experimental].py
+++ streamlit/pages/4_[Experimental].py
@@ -11,6 +11,12 @@ def import_viz_data():
     data = pd.read_csv("./data/viz.csv").drop("Unnamed: 0", axis = 1)
     return data
 
+def fill_anova_dict():
+    groups = data[group_to_compare].unique()
+    for i in groups:
+        anova_dict[i] = data[data[group_to_compare] == i][var_to_examine]
+    return anova_dict
+
 #Load data
 data = import_viz_data()
 
@@ -30,13 +36,17 @@ with st.container(border=True):
         
     group_to_compare = st.selectbox(label="Groups to compare", options=group_to_compare_choice).lower().replace(" ", "_")
     
-anova_dict = dict()
-def fill_anova_dict():
-    groups = data[group_to_compare].unique()
-    for i in groups:
-        anova_dict[i] = data[data[group_to_compare] == i][var_to_examine]
-    return anova_dict
+    anova_submit = st.button(label="Run ANOVA")
+    
+if not anova_submit:
+    with st.container(border=True):
+        st.subheader("Click run to see results!")
+
+if anova_submit:
+    anova_dict = dict()
+
 
 anova_dict = fill_anova_dict()
 
 st.text(anova_dict)
+    
