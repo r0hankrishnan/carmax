ac0d789bf7ba81ff30530c22ae0152603d49a0d8
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-08-29 22:49:09 -0400

    Update test page

--- streamlit/pages/4_[Experimental].py
+++ streamlit/pages/4_[Experimental].py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
+st.warning("This page is just here to test various layouts for the three main pages of this dashboard. Please ignore!")
+
+
 @st.cache_data
 def import_viz_data():
     data = pd.read_csv("./data/viz.csv").drop("Unnamed: 0", axis = 1)
